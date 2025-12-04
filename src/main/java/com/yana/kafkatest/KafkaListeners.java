fe7cda954f914c614586b1031f6aed523aa07dc4
TestingMyself0 <workspace01001@gmail.com>
2025-12-04 20:19:46 +0300

    kafka works with string

---
+++ src/main/java/com/yana/kafkatest/KafkaListeners.java
@@ -0,0 +1,17 @@
+package com.yana.kafkatest;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(
+            topics = "firstTopic",
+            groupId = "groupId"
+    )
+    void listener(String data) {
+        System.out.println("Listener received: " + data);
+    }
+
+}
---
+++ src/main/java/com/yana/kafkatest/MessageRequest.java
@@ -0,0 +1,6 @@
+package com.yana.kafkatest;
+
+public record MessageRequest(
+        String message
+) {
+}
